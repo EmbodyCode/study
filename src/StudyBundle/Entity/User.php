5f2e908b549dfa0712803c62a338907004dcfd07
toxic <tox1ctox@mail.ru>
2017-02-18 21:49:54 +0300

    Added two fields for BaseUserType and realized secured area '/'

--- src/StudyBundle/Entity/User.php
+++ src/StudyBundle/Entity/User.php
@@ -16,8 +16,35 @@ class User extends BaseUser
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
+    
+    /**
+     * @ORM\Column(type="string", name="firstName", length=255)
+     */
+    private $firstName;
+    
+    /**
+     * @ORM\Column("type=string", name="lastName", length=255)
+     */
+    
+    private $lastName;
+    
+    public function getFirstName() {
+        return $this->firstName;
+    }
 
-    public function __construct()
+     public function getLastName() {
+        return $this->lastName;
+    }
+
+     public function setFirstName($firstName) {
+        $this->firstName = $firstName;
+    }
+
+     public function setLastName($lastName) {
+        $this->lastName = $lastName;
+    }
+
+        public function __construct()
     {
         parent::__construct();
         // your own logic
